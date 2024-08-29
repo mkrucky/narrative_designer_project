5c5152551198d0c0fd7f0c63c29fb8b9dfbcce14
unknown <mrkrucky@gmail.com>
2024-08-29 15:27:17 +0200

    Adding new templates

---
+++ main/static/main/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('welcome-btn').addEventListener('click', function() {
+        document.getElementById('welcome-message').style.display = 'none';
+    });
+});
--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about_me, name='about_me'),
+    path('projects/', views.projects, name='projects'),
+    path('contact/', views.contact, name='contact'),
 ]
